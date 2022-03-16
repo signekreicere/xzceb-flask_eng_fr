21230638fa33885cd47504ac14750ad75f5432a3
signekeicere <signe.kreicere@gmail.com>
2022-03-16 01:25:17 +0000

    Final changes

---
+++ final_project/machinetranslation/translator.py
@@ -0,0 +1,29 @@
+import json
+"This file is for translating"
+import os
+from ibm_watson import LanguageTranslatorV3
+from ibm_cloud_sdk_core.authenticators import IAMAuthenticator
+from dotenv import load_dotenv
+
+load_dotenv()
+
+apikey = os.environ['apikey']
+url = os.environ['url']
+authenticator = IAMAuthenticator(apikey)
+language_translator = LanguageTranslatorV3(
+    version='2018-05-01',
+    authenticator=authenticator
+)
+language_translator.set_service_url(url)
+
+def english_to_french(english_text):
+    "Translate english to french"
+    result = language_translator.translate(text=english_text, model_id='en-fr').get_result()
+    french_text = result['translations'][0]['translation']
+    return french_text
+
+def french_to_english(french_text):
+    "Translate french to english"
+    result = language_translator.translate(text=french_text, model_id='fr-en').get_result()
+    english_text = result['translations'][0]['translation']
+    return english_text
